4e90cfcd96e02ea0adf276c055746b6562d9662d
randomizer333 <markosmid333@gmail.com>
2016-12-12 23:18:16 +0100

    Update dropdownfill.php

--- dropdownfill.php
+++ dropdownfill.php
@@ -1,13 +1,11 @@
-<input name="enota[]"
-						list="enote"				
-						class="enota" >
-            
-							<datalist id="enote">
-								<?php //preberi obstoječe enote
-									$q_enote="SELECT enota FROM cookbook.sestavina GROUP BY enota;";
-									$enote_result=mysqli_query($con,$q_enote);
-									while($enote = mysqli_fetch_array($enote_result)){
-										echo "<option value=$enote[0]>";
-									}	
-								?>			
-							</datalist>
+<input name="input[]" list="options" class="in.field" >      
+	<datalist id="field">
+	<?php 			//get existing values from table
+		function ddfill ( $column, $table ){
+		$querry = "SELECT $column FROM $table GROUP BY $column ;";
+		$querry_result = mysqli_query ( $connection, $querry );
+		while ( $field = mysqli_fetch_array ( $querry_result )){
+			echo "<option value=$field[0]>";
+			}	
+	?>			
+	</datalist>
