29d623b786dea1cdecdb68ae0bb49cbcca6caa71
randomizer333 <markosmid333@gmail.com>
2016-12-16 14:59:31 +0100

    Create functionParameterCheck.php

---
+++ php/functionParameterCheck.php
@@ -0,0 +1,22 @@
+// function parameter number check and error return
+
+
+
+<?php 
+$noParam = array("red", "green", "blue", "yellow"); 
+
+foreach ($colors as $value) {
+    echo "$value <br>";
+}
+?>
+
+
+function fpcheck ( param1, param2, param3 ){  //
+    for ( $x = 0 )
+}
+
+<?php 
+for ($x = 0; $x <= 10; $x++) {
+    echo "The number is: $x <br>";
+} 
+?>
