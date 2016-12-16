f13bac45b11c270b26dc3be10d78ade28f912f88
randomizer333 <markosmid333@gmail.com>
2016-12-16 10:34:16 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,13 +1,19 @@
 <?php
-function connect ( $host, $user, $pass )
+//Input:
+function connect ( $host, $user, $pass ) 
 {
-  $host = "localhost";
-  $user = "username";
+  for (){   // function parameter number check and error return
+  }
+  
+  $host = "localhost";  //replace for hostname or servername usualy localhost or name of sqlserver
+  $user = "username"; //login credentials for sql database connection
   $pass = "password";
 
-  // Create connection
-  $conn = mysqli_connect($servername, $username, $password);
+//Process:
+// Create connection
+  $conn = mysqli_connect($host, $user, $pass);
 
+//Output:
   // Check connection
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
