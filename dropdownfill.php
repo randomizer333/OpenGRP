ee6fd74761839c58fc6e530e5141598329485f9a
randomizer333 <markosmid333@gmail.com>
2016-12-27 23:15:11 +0100

    Update dropdownfill.php

--- dropdownfill.php
+++ dropdownfill.php
@@ -1,11 +1,11 @@
 <input name="input[]" list="options" class="in.field" >      
 	<datalist id="field">
-	<?php 			//get existing values from table
-		function ddfill ( $column, $table ){
-		$querry = "SELECT $column FROM $table GROUP BY $column ;";
-		$querry_result = mysqli_query ( $connection, $querry );
-		while ( $field[] = mysqli_fetch_array ( $querry_result )){
-			echo "<option value=$field[0]>";
-			}	
-	?>			
+		<?php 			//get existing values from table
+			function ddfill ( $column, $table ){
+			$querry = "SELECT $column FROM $table GROUP BY $column ;";
+			$querry_result = mysqli_query ( $connection, $querry );
+			while ( $field[] = mysqli_fetch_array ( $querry_result )){
+				echo "<option value=$field[0]>";
+				}	
+		?>			
 	</datalist>
